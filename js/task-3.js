2be02b494a04d0e6302555d1d6105fce9a57a7ff
OleksandrNol1k <nevskij.mail@gmail.com>
2025-03-26 22:44:28 +0200

    final

--- js/task-3.js
+++ js/task-3.js
@@ -2,5 +2,5 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 nameInput.addEventListener('input', () => {
     const trimmedValue = nameInput.value.trim();
-    output.textContent = trimmedValue ? trimmedValue : "Anonymous";
+    nameOutput.textContent = trimmedValue ? trimmedValue : "Anonymous";
 });
